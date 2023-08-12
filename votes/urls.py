813059fb500e2dcb7f978fc5d229736089bdc74c
alvian <alvian@metalit.id>
2023-08-12 17:13:06 +0700

    result as api

--- votes/urls.py
+++ votes/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("result/<uuid:question_id>", views.result, name="result"),
 ]
--- votes/views.py
+++ votes/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from . import models
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the votes index.")
+
+
+@api_view(['GET'])
+def result(request, question_id):
+    
+    if request.method == 'GET':
+        question = models.Question.objects.get(id=question_id)
+        return Response(question.get_choice_percentages())
